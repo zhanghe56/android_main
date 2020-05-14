77f5fb1729c8c1928491c769b01ee951874b4cae
ZH <7418782+zhanghe56@user.noreply.gitee.com>
2020-05-14 20:41:44 +0800

    update jsall.js.

--- jsall.js
+++ jsall.js
@@ -60,3 +60,22 @@ window.XB.actionFromJsWithParam("当前版本号：" + sessionStorage.visioncode
 	}
 	}
 }
+
+function vscodeFunction(){
+function Request(strName){
+var strHref = location.href;
+var intPos = strHref.indexOf("?");
+var strRight = strHref.substr(intPos + 1);
+var arrTmp = strRight.split("&");
+for(var i = 0; i < arrTmp.length; i++) {
+var arrTemp = arrTmp[i].split("=");
+if(arrTemp[0].toUpperCase() == strName.toUpperCase()) return arrTemp[1];
+}
+return "";
+}
+var id=Request("id");
+if(id=="")
+
+else
+sessionStorage.visioncode=(id);
+}
