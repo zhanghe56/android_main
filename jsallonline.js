8c06fcf482a4d032f17bcc0991542c047d92828b
zhanghe56 <18744355478@163.com>
2020-05-31 17:13:17 +0800

    更新至2.0.0531rev1.42版本

--- jsallonline.js
+++ jsallonline.js
@@ -1,7 +1,7 @@
 function checkvision() {
-sessionStorage.newvision=("41");
-sessionStorage.updateday=("2020.05.29");
-sessionStorage.newvisionname=("2.0.0529rev1");
+sessionStorage.newvision=("42");
+sessionStorage.updateday=("2020.05.31");
+sessionStorage.newvisionname=("2.0.0531rev1");
 window.XB.actionFromJsWithParam('当前版本号' + sessionStorage.visionname + "." + sessionStorage.visioncode);
  if (sessionStorage.visioncode==sessionStorage.newvision) {
      if(sessionStorage.visionname.indexOf(sessionStorage.newvisionname) !=-1){
